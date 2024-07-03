6e0271bd015b095976fd121fb3aa2fdd3a2ea49f
BugMakerH <1371444344@qq.com>
2024-07-03 16:30:12 +0100

    Add dropout layer to Mamba

--- models/Mamba.py
+++ models/Mamba.py
@@ -13,12 +13,20 @@ class Mamba(nn.Module):
         self.d_conv = config['d_conv']
         self.expand = config['expand']
 
+        self.dropout = config['dropout']
+
         self.embedding = nn.Embedding(num_tokens, self.d_model)
+        self.dropout1 = nn.Dropout(self.dropout)
         self.mamba = Mamba_ssm(self.d_model, self.d_state, self.d_conv, self.expand)
+        self.dropout2 = nn.Dropout(self.dropout)
         self.head = nn.Linear(self.d_model, num_tokens)
+        self.dropout3 = nn.Dropout(self.dropout)
 
     def forward(self, x):
         x = self.embedding(x)
+        x = self.dropout1(x)
         x = self.mamba(x)
+        x = self.dropout2(x)
         x = self.head(x)
+        x = self.dropout3(x)
         return x[:, -1, :]

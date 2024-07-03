a61bc6805f0421fba5eac81ee3a636ed533999e9
BugMakerH <1371444344@qq.com>
2024-07-03 15:10:33 +0100

    Add Mamba model

---
+++ models/Mamba.py
@@ -0,0 +1,24 @@
+from torch import nn
+from mamba_ssm import Mamba
+
+
+class GPMamba(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        num_tokens = config['num_tokens']
+
+        self.d_model = config['d_model']
+        self.d_state = config['d_state']
+        self.d_conv = config['d_conv']
+        self.expand = config['expand']
+
+        self.embedding = nn.Embedding(num_tokens, self.d_model)
+        self.mamba = Mamba(self.d_model, self.d_state, self.d_conv, self.expand)
+        self.head = nn.Linear(self.d_model, num_tokens)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.mamba(x)
+        x = self.head(x)
+        return x[:, -1, :]

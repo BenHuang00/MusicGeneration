7a29771aa33572687e38558835d80f220ba67712
BugMakerH <1371444344@qq.com>
2024-07-29 16:05:01 +0100

    feat(Mamba2.py): add a new module Mamba2 to implement a custom neural network architecture Mamba

---
+++ models/Mamba2.py
@@ -0,0 +1,30 @@
+from torch import nn
+from mamba_ssm import Mamba2 as Mamba_ssm
+
+
+class Mamba(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.num_tokens = config['num_tokens']
+
+        self.d_model = config['d_model']
+        self.d_state = config['d_state']
+        self.d_conv = config['d_conv']
+        self.expand = config['expand']
+
+        self.dropout = config['dropout']
+
+        self.embedding = nn.Embedding(self.num_tokens, self.d_model)
+        self.dropout1 = nn.Dropout(self.dropout)
+        self.mamba = Mamba_ssm(self.d_model, self.d_state, self.d_conv, self.expand)
+        self.dropout2 = nn.Dropout(self.dropout)
+        self.fc = nn.Linear(self.d_model, self.num_tokens)
+
+    def forward(self, src, tgt):
+        src = self.embedding(src)
+        src = self.dropout1(src)
+        src = self.mamba(src)
+        src = self.dropout2(src)
+        out = self.fc(src[:, -1, :])
+        return out
